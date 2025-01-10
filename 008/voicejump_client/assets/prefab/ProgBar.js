f35db0d42f26ec25c1038808229bfd0e4d87fce9
lingjijian <lingjijian@live.com>
2025-01-10 10:34:46 +0800

    Update

--- 008/voicejump_client/assets/prefab/ProgBar.js
+++ 008/voicejump_client/assets/prefab/ProgBar.js
@@ -23,12 +23,14 @@ cc.Class({
         that.node.active = true;
         for (let i = 0; i < globalData.gameMgr.playerData.length; i++) {
             this['avator' + i].node.active = true;
-            cc.loader.load(globalData.gameMgr.playerData[i].avatorUrl, function (err, img) {
-                if (!err) {
-                    that['avator' + i].spriteFrame = new cc.SpriteFrame(img);
-                    that['avator' + i].node.x = 0;
-                }
-            });
+            if(globalData.gameMgr.playerData[i].avatorUrl){
+                cc.loader.load(globalData.gameMgr.playerData[i].avatorUrl, function (err, img) {
+                    if (!err) {
+                        that['avator' + i].spriteFrame = new cc.SpriteFrame(img);
+                        that['avator' + i].node.x = 0;
+                    }
+                });
+            }
         }
     },
     refresh: function () {
