29976bff4387d148eff657a3b81afe4048b758ed
zhengqihang <2281876948@qq.com>
2025-07-01 05:19:38 +0000

    编辑syncCode.js

--- game-update/syncCode.js
+++ game-update/syncCode.js
@@ -22,6 +22,21 @@ app.post('/syncgame', upload.single('file'), (req, res) => {
     res.send(req.file.filename+' 文件上传成功');
     const zipFile = './upload/'+req.file.filename;
 
+        //五子棋上传路径
+        const destFolder_001 = '/sunday/lianjin_game/game-update/game-wzq';
+        //斗地主上传路径
+        const destFolder_002 = '/sunday/lianjin_game/game-update/game-hlddz';
+        // UNO上传路径
+        const destFolder_003 = '/sunday/lianjin_game/game-update/game-uno';
+        // 中国象棋上传路径
+        const destFolder_005 = '/sunday/lianjin_game/game-update/game-zgxq';
+        // 飞行棋上传路径
+        const destFolder_006 = '/sunday/lianjin_game/game-update/game-fxq';
+        // 大富翁上传路径
+        const destFolder_007 = './games/monopoly';
+        // 跳动小鸟上传路径
+        const destFolder_008 = '/sunday/lianjin_game/game-update/game-voicejump';
+
     const destFolder = './upload';
 
     if(req.file.filename == "001.zip"){
