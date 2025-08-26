35fb2df6eb6870ab669c55c2f86329fd3fe33544
Jay.L <oncwnuEX88CSvdtp_ODQjgbG1W3w@git.weixin.qq.com>
2025-08-26 09:52:17 +0800

    Update

--- 003/uno_client/assets/Prefab/PanelSetting.js
+++ 003/uno_client/assets/Prefab/PanelSetting.js
@@ -1,11 +1,36 @@
+
 cc.Class({
     extends: cc.Component,
     name:"PanelSetting",
     properties: {
+        bgMucic:cc.AudioSource,
         // progressbar_bg:cc.ProgressBar,
         // progressbar_effect:cc.ProgressBar,
     },
-   
+    // start:function(){
+    //     if (cc.sys.isBrowser && cc.sys.os === cc.sys.OS_IOS && cc.sys.isMobile) {
+
+    //         cc.game.on(cc.game.EVENT_SHOW, () => {
+    //             setTimeout(() => {
+    //                 cc.audioEngine.stopMusic();
+    //             }, 50);
+    //             setTimeout(() => {
+    //                 cc.playMusic("sound/bg",true,1);
+    //             }, 100);
+    //         });
+    //     }
+    // },
+    // onBtnReset:function(){
+    //     setTimeout(() => {
+    //         cc.audioEngine.stopAll();
+    //         cc.audioEngine.stopMusic();
+
+    //         // cc.audioEngine.stopAllEffects();
+    //     }, 50);
+    //     setTimeout(() => {
+    //         cc.playMusic("sound/bg",true,1);
+    //     }, 100);
+    // },
     onBtnClose:function(){
         this.node.active = false;
     },
@@ -17,12 +42,14 @@ cc.Class({
         if(toggle.isChecked){
             cc.isPlayingGlobalBg = 1;
             // cc.audioEngine.setMusicVolume(1);
-            cc.audioEngine.stopMusic();
-            cc.playMusic("sound/bg",true,1);
+            // cc.audioEngine.stopMusic();
+            // cc.playMusic("sound/bg",true,1);
+            this.bgMucic.volume = 1;
         }else{
             cc.isPlayingGlobalBg = 0;
             // cc.audioEngine.setMusicVolume(0.01);
-            cc.audioEngine.stopMusic();
+            // cc.audioEngine.stopMusic();
+            this.bgMucic.volume = 0;
         }
     },
     onToggleEffect:function(toggle, customEventData){
@@ -35,4 +62,12 @@ cc.Class({
             cc.audioEngine.setEffectsVolume(0.01);
         }
     },
+    // onProgressBg:function(slider) {
+    //     this.progressbar_bg.progress = slider.progress;
+    //     cc.audioEngine.setMusicVolume(slider.progress);
+    // },
+    // onProgressEffect:function(slider) {
+    //     this.progressbar_effect.progress = slider.progress;
+    //     cc.audioEngine.setEffectsVolume(slider.progress);
+    // }
 });
