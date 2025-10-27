b33a2c528ae61de7a2e140a47277b3935d382f7d
Jay.L <oncwnuEX88CSvdtp_ODQjgbG1W3w@git.weixin.qq.com>
2025-10-27 18:31:36 +0800

    Update

--- 003/uno_server/index.js
+++ 003/uno_server/index.js
@@ -451,8 +451,7 @@ const proto = {
       score_list:desk.score_list,
     });
   },
-  makePass:function(desk,curPosId){
-    console.log("makePass")
+  _getPlusNum:function(desk){
     var plusNum = 0;
     var last_card = desk.out_cards[desk.out_cards.length - 1];
     if(!last_card) return;
@@ -480,6 +479,30 @@ const proto = {
     }else{
       plusNum = 1;
     }
+    return plusNum;
+  },
+   _getPlusPrepareNum:function(desk){
+    var plusNum = 0;
+    var last_card = desk.out_cards[desk.out_cards.length - 1];
+    if(!last_card) return 0;
+
+    for (let i = desk.out_cards.length - 1; i >= 0; i--) {
+      var v = desk.out_cards[i];
+      if(v.value == 'plus2' ){
+        if(!v.mark){
+          plusNum += 2;
+        }
+      }else if (v.value == 'plus4'){
+        if(!v.mark){
+          plusNum += 4;
+        }
+      }
+    }
+    return plusNum;
+  },
+  makePass:function(desk,curPosId){
+    console.log("makePass")
+    var plusNum = this._getPlusNum(desk);
 
     var userObj = desk.positions[curPosId];
     var nextPosId = this.getNextPosId(desk,curPosId);
@@ -919,7 +942,8 @@ const proto = {
               new_cards.push(_card);
             }
             desk.positions[curPosId].cards = new_cards;
-            self.broadCastRoom("PLAY_CARD_SUCCESS",desk.deskId,{card:obj,posId:curPosId,nextPosId:nextPosId,server_time:getTimeStamp()})
+            
+            self.broadCastRoom("PLAY_CARD_SUCCESS",desk.deskId,{card:obj,posId:curPosId,nextPosId:nextPosId,server_time:getTimeStamp(),plus_num:self._getPlusPrepareNum(desk)})
             desk.hadExecutePlayCard = false;
             desk.time_out = 30;
             console.log("已经游玩了："+(getTimeStamp() - desk.start_time) +"秒");
