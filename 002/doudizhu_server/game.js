ba8da84c7d2efb148566256b0423a6fd65a39812
lianjin <lianjin@qq.com>
2025-12-01 12:30:18 +0800

    Update

--- 002/doudizhu_server/game.js
+++ 002/doudizhu_server/game.js
@@ -63,9 +63,9 @@ Object.assign(
           
           var offset = getRandomNumForRange(maxIndex);
           //debug
-          if(i == 0){
-            offset = 0;
-          }
+          // if(i == 0){
+          //   offset = 0;
+          // }
           group.push(mCards[offset]);
           mCards.splice(offset, 1);
           maxIndex--;
@@ -181,8 +181,13 @@ Object.assign(
               if( this.lastCardInfo.len == 4){
                   if(isAAAAMe){
                       return {
-                              status: false,
-                          }
+                                  status: true,
+                                  key,
+                                  type,
+                                  len: ret.len,
+                                  is_laizi:is_laizi,
+                                  isAAAA:ret.isAAAA,
+                              }
                    }else{
                         if( key > this.lastCardInfo.key){
                             return {
